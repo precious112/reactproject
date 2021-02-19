febad19ddc507c62059efe16c809a34d7bf24d1d
precious112 <preciouskent8@gmail.com>
2021-02-18 18:30:41 -0800

    Updated FuckingApp

--- reactproject/settings.py
+++ reactproject/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
